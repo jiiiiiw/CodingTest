d604d63863051ac2ca0a6964c33749a8d24d6bf6
jiiiiiw <98935125+jiiiiiw@users.noreply.github.com>
2024-07-22 12:57:10 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 1.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -1,36 +1,34 @@
 def solution(bandage, health, attacks):
-    now_health = health 
-    attack_time = [i[0] for i in attacks]
-    dealing = [i[1] for i in attacks]
-    max_time = max([i[0] for i in attacks]) 
-    bandage_time = bandage[0] 
-    second_healing = bandage[1] 
-    more_healing = bandage[2] 
-    cnt = 0
-    for time in range(1,max_time+1):
-        if time in attack_time: 
-            cnt = 0 
-            now_health -= dealing[attack_time.index(time)] 
-            if now_health <= 0: 
+    now_health = health  # 초기 체력 설정
+    attack_time = [i[0] for i in attacks]  # 공격 발생 시간 리스트
+    dealing = [i[1] for i in attacks]  # 각 공격의 데미지 리스트
+    max_time = max([i[0] for i in attacks])  # 가장 늦게 발생하는 공격 시간
+    bandage_time = bandage[0]  # 밴드 사용 시간 간격
+    second_healing = bandage[1]  # 매 초당 치유량
+    more_healing = bandage[2]  # 추가 치유량
+    cnt = 0  # 밴드 사용 시간 간격을 추적하는 카운터
+    
+    for time in range(1, max_time + 1):  # 1초부터 max_time까지의 시간 루프
+        if time in attack_time:  # 현재 시간이 공격 시간인 경우
+            cnt = 0  # 카운터 리셋
+            now_health -= dealing[attack_time.index(time)]  # 데미지 적용
+            if now_health <= 0:  # 체력이 0 이하인 경우
                 answer = -1
-                return answer
-        else:
-            cnt +=1
-            if cnt == bandage_time:
+                return answer  # 캐릭터 사망, -1 반환
+        else:  # 현재 시간이 공격 시간이 아닌 경우
+            cnt += 1
+            if cnt == bandage_time:  # 밴드 사용 시간 간격이 된 경우
                 cnt = 0
-                now_health += more_healing
-                if now_health >= health :
+                now_health += more_healing  # 추가 치유 적용
+                if now_health >= health:  # 최대 체력을 초과하지 않도록 조정
                     now_health = health
-            if now_health == health: 
-                pass
-            else: 
-                now_health += second_healing
-                if now_health >= health:
+            if now_health != health:  # 현재 체력이 최대 체력이 아닌 경우
+                now_health += second_healing  # 매 초당 치유량 적용
+                if now_health >= health:  # 최대 체력을 초과하지 않도록 조정
                     now_health = health
-    print(now_health)
-
+    
     if now_health <= 0:
         answer = -1
     else:
         answer = now_health
-    return answer
+    return answer  # 최종 체력 반환
