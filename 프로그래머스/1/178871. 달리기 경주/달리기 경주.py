b5b1e4f8ca5fde80b5103250408a46d112589635
jiiiiiw <98935125+jiiiiiw@users.noreply.github.com>
2024-07-22 12:27:49 +0900

    [level 1] Title: 달리기 경주, Time: 962.25 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -1,15 +1,17 @@
 def solution(players, callings):
-    players_dict = dict()
-    for idx, values in enumerate(players):
-        players_dict[values] = idx
+    # callings에서 나오는 이름을 players에서 탐색 하지 않는다. 시간 너무 많아
+    # name:rank의 딕셔너리를 만들어서 좋은 시간효율을 뽑아내버리자
+    name2rank = {name:rank for rank, name in enumerate(players)}
     
-    for call in callings:
-        rank = players_dict[call]
-        
-        players_dict[call] -=1
-        players_dict[players[rank-1]] +=1
-        
-        players[rank-1], players[rank] = call, players[rank-1]
-                
-    answer = players
-    return answer
+    # print(name2rank)
+    
+    for name in callings:
+        # 현재 호명된 선수의 등수 가져오기
+        rank = name2rank[name]
+        # 추월하는 로직을 구현
+        players[rank-1], players[rank] = players[rank], players[rank-1] # 스와핑
+        #r2n도 업데이트
+        name2rank[players[rank]] = rank
+        name2rank[players[rank-1]] = rank-1
+    
+    return players
