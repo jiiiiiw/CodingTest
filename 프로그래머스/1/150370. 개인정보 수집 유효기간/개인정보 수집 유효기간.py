76c1fc354d347ec184a1ea9f1977c4734b7bc44a
jiiiiiw <98935125+jiiiiiw@users.noreply.github.com>
2024-07-22 12:15:01 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.25 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,19 +1,19 @@
 def solution(today, terms, privacies):
-    d ={}
+    ty, tm, td = map(int, today.split("."))
+    # print(ty, tm, td)
+    today = ty * (12 * 28) + tm * 28 + td # 현재시점을 day 단위로 변환
+    
+    t2m = {tm.split()[0]:int(tm.split()[1]) for tm in terms}
+    
     answer = []
-    today_lst = list(map(int,today.split('.')))
     
-    for term in terms: 
-        n, m = term.split()
-        d[n] = int(m)*28 
-    
-    for i in range(len(privacies)):
-        date, s = privacies[i].split()
-        date_lst = list(map(int, date.split('.'))) 
-        year = (today_lst[0] - date_lst[0])*336 
-        month = (today_lst[1] - date_lst[1])*28 
-        day = today_lst[2] - date_lst[2]
-        total = year+month+day
-        if d[s] <= total:
-            answer.append(i+1)
+    # print(t2m)
+    for idx, privacy in enumerate(privacies): # 인덱스와 값에 대한 튜플을 생성
+        start, term = privacy.split()
+        # print(start,term)
+        sy, sm, sd = map(int, start.split("."))
+        sday = sy * (12 * 28) + sm * 28 + sd # 현재시점을 day 단위로 변환
+        tmday = t2m[term] * 28
+        if not today < sday + tmday: # 참이 안전한 날, 거짓이 result로
+            answer.append(idx+1)        
     return answer
