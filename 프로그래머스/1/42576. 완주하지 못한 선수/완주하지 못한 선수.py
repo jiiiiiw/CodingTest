f2b809ff0d19c212674e0b9b91d43e7bed41d350
jiiiiiw <98935125+jiiiiiw@users.noreply.github.com>
2025-06-04 16:58:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 61.49 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,8 +1,19 @@
 def solution(participant, completion):
-    participant.sort()
-    completion.sort()
+    #participant.sort()
+    #completion.sort()
     
-    for str1, str2 in zip(participant, completion):
-        if str1 != str2:
-            return str1
-    return participant[-1]
+    #for str1, str2 in zip(participant, completion):
+    #    if str1 != str2:
+    #        return str1
+    #return participant[-1]
+    
+    temp = 0
+    dic = {}
+    
+    for p in participant:
+        dic[hash(p)] = p
+        temp += int(hash(p))
+    for c in completion:
+        temp -= int(hash(c))
+    
+    return dic[temp]
