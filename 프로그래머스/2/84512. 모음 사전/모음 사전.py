6c66cdbc89dadff3dbe8866275db137c922b3ba9
jiiiiiw <98935125+jiiiiiw@users.noreply.github.com>
2025-06-22 20:16:11 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,15 +1,8 @@
-def solution(word):
-    answer = 0
-    word_list = []
-    words = 'AEIOU'
-    
-    def dfs(cnt, w):
-        if cnt == 5:
-            return False
-        for i in range(len(words)):
-            word_list.append(w + words[i])
-            dfs(cnt+1, w + words[i])
-            
-    dfs(0,"")
-    
-    return word_list.index(word)+1
+def solution(w):
+    a = ['A', 'E', 'I', 'O', 'U']
+    p = [781, 156, 31, 6, 1]  # 자리별 가중치
+
+    r = 0
+    for i, c in enumerate(w):
+        r += a.index(c) * p[i] + 1
+    return r
