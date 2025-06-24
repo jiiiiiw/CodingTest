75ffb32f64fdd15ec5a9a8cbdf4e59a43f2d6ba3
jiiiiiw <98935125+jiiiiiw@users.noreply.github.com>
2025-06-24 15:07:53 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.76 ms, Memory: 9.49 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 29.08 ms
+메모리: 9.49 MB, 시간: 12.76 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 05일 11:18:54
+2025년 06월 24일 15:07:50
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,30 +1,21 @@
-from heapq import heappush, heappop
+from collections import deque
 
-def solution(maps):
-    R, C = len(maps), len(maps[0])
-    
-    maps = [[float("inf") if maps[r][c] else 0 for c in range(C)] for r in range(R)] # 지나온 1과 새로운 1을 구분해주기 위한 무한으로의 표현
-    # print(maps)
-    
-    # [1,0,9,10,11]
-    # [2,0,8,0,inf]
-    # [3,0,7,8,9]
-    # [4,5,6,0,10]
-    # [0,0,0,0,11] -> flooding 알고리즘을 활용해보자
-    
-    maps[0][0] = 1
-    QUEUE = []
-    heappush(QUEUE, (1,0,0)) # 몇칸, r, c
-    # print(QUEUE)
-    
-    while QUEUE:
-        cv, cr, cc = heappop(QUEUE)
-        for rd, cd in ((-1,0),(0,1),(1,0),(0,-1)):
-            nr, nc = cr + rd, cc + cd
-            if nr in range(R) and nc in range(C) and maps[nr][nc]:
-                if cv + 1 < maps[nr][nc]:
-                    maps[nr][nc] = cv + 1
-                    heappush(QUEUE, (maps[nr][nc],nr,nc))
-    return -1 if maps[R-1][C-1] == float("inf") else maps[R-1][C-1]
-                    
-    # bfs 를 큐를 통해 구현한 모습
+def solution(m):
+    n, k = len(m), len(m[0])
+    v = [[0]*k for _ in range(n)]
+    q = deque()
+    q.append((0, 0))
+    v[0][0] = 1
+
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            a, b = x + dx[i], y + dy[i]
+            if 0 <= a < n and 0 <= b < k and m[a][b] == 1 and v[a][b] == 0:
+                v[a][b] = v[x][y] + 1
+                q.append((a, b))
+
+    return v[n-1][k-1] if v[n-1][k-1] != 0 else -1
