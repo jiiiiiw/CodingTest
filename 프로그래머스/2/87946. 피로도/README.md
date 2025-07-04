1357f4ee83c1d5f4b1cdd2426ca38e113b3022af
jiiiiiw <98935125+jiiiiiw@users.noreply.github.com>
2025-07-05 04:30:41 +0900

    [level 2] Title: 피로도, Time: 30.86 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 44.65 ms
+메모리: 9.12 MB, 시간: 30.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 05일 12:11:30
+2025년 07월 05일 04:30:39
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,15 +1,15 @@
-def solution(k, dungeons):
-    def dfs(k, dungeons, visited, count):
-        nonlocal max_count
-        max_count = max(max_count, count)
-        
-        for i in range(len(dungeons)):
-            if not visited[i] and k >= dungeons[i][0]:
-                visited[i] = True
-                dfs(k - dungeons[i][1], dungeons, visited, count + 1)
-                visited[i] = False
-    
-    max_count = 0
-    visited = [False] * len(dungeons)
-    dfs(k, dungeons, visited, 0)
-    return max_count
+from itertools import permutations
+
+def solution(k, d):
+    m = 0
+    for p in permutations(d):
+        t = k  
+        c = 0  
+        for a, b in p:
+            if t >= a:
+                t -= b
+                c += 1
+            else:
+                break
+        m = max(m, c)
+    return m
