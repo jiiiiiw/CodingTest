249b6cdc450b2178cf004985cc15fcf77b697a3e
jiiiiiw <98935125+jiiiiiw@users.noreply.github.com>
2025-07-24 11:44:13 +0900

    [level 2] Title: 타겟 넘버, Time: 401.24 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,16 +1,16 @@
-def dfs(numbers, target_num, curr_sum, curr_idx):
-    if curr_idx == len(numbers):
-        # 타케트가 만들어지는 경우
-        if curr_sum == target_num:
-            return 1
-        else:
-            return 0
-    else:
-        answer = 0
-        answer += dfs(numbers, target_num, curr_sum+numbers[curr_idx], curr_idx+1)
-        answer += dfs(numbers, target_num, curr_sum-numbers[curr_idx], curr_idx+1)
-        return answer
-
 def solution(numbers, target):
-    answer = dfs(numbers, target, 0, 0)
+    answer = 0
+
+    def dfs(index, current_sum):
+        nonlocal answer
+        # 모든 숫자를 다 사용했을 때
+        if index == len(numbers):
+            if current_sum == target:
+                answer += 1
+            return
+        dfs(index + 1, current_sum + numbers[index])
+
+        dfs(index + 1, current_sum - numbers[index])
+
+    dfs(0, 0)
     return answer
