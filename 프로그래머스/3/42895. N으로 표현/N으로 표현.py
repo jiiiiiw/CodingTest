c41dadefa9dfac98698d906698616e0cbf0f40e0
jiiiiiw <98935125+jiiiiiw@users.noreply.github.com>
2025-09-19 14:49:17 +0900

    [level 3] Title: N으로 표현, Time: 17.09 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -2,19 +2,25 @@ def solution(N, number):
     if N == number:
         return 1
     
-    dp = [set() for _ in range(9)]  
-
-    for i in range(1, 9):
-        dp[i].add(int(str(N) * i))
-        for j in range(1, i):
-            for a in dp[j]:
-                for b in dp[i - j]:
-                    dp[i].add(a + b)
-                    dp[i].add(a - b)
-                    dp[i].add(a * b)
+    dp = [set() for _ in range(9)]
+    
+    for k in range(1,9):
+        dp[k].add(int(str(N) * k))
+        
+        for i in range(1, k):
+            for a in dp[i]:
+                for b in dp[k-i]:
+                    dp[k].add(a + b)
+                    dp[k].add(a - b)
+                    dp[k].add(a * b)
                     if b != 0:
-                        dp[i].add(a // b)
-        if number in dp[i]:
-            return i
-
+                        dp[k].add(a // b)
+        
+        print(f"k={k}, count={len(dp[k])}, sample={list(dp[k])[:10]}")
+        
+        if number in dp[k]:
+            print(f"==> number {number} found at k={k}")
+            return k
+    
     return -1
+            
--- 프로그래머스/3/42895. N으로 표현/README.md
+++ 프로그래머스/3/42895. N으로 표현/README.md
@@ -1,10 +1,10 @@
 # [level 3] N으로 표현 - 42895 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 16.98 ms
+메모리: 10.4 MB, 시간: 17.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 01일 15:18:36
+2025년 09월 19일 14:49:14
 
 ### 문제 설명
 
